8d531815b103236c881b8f78173bdba9f12c86a5
nilknarF <nilknarF@DESKTOP-SBFK8GR>
2021-02-25 19:06:55 +0800

    demo

---
+++ PythonProject/Demo.py
@@ -0,0 +1,14 @@
+'''
+Created on Feb 13, 2021
+
+@author: nilknarF
+'''
+import time
+
+name = 'frank'
+age = input('what is your age: '  )
+print(f'hello  {name} and you are {age} years old ')
+time.sleep(5)
+print('end')
+
+
