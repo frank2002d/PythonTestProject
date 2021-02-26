1beb77f18c345154eb7cf16b34b74d01732d2751
frank2002d <63859720+frank2002d@users.noreply.github.com>
2021-02-26 08:37:24 +0800

    Update Demo.py

--- PythonProject/Demo.py
+++ PythonProject/Demo.py
@@ -5,10 +5,8 @@ Created on Feb 13, 2021
 '''
 import time
 
-name = 'frank'
+name = input('what is your name? ')
 age = input('what is your age: '  )
 print(f'hello  {name} and you are {age} years old ')
 time.sleep(5)
 print('end')
-
-
